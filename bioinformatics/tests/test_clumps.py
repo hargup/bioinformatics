0fc4e4d3cbe7cc4677ea19a49a22690b4d18b4f1
Harsh Gupta <gupta.harsh96@gmail.com>
2014-10-09 01:23:01 +0530

    add another test to test_clumps

--- bioinformatics/tests/test_clumps.py
+++ bioinformatics/tests/test_clumps.py
@@ -5,3 +5,10 @@ def test_find_clums():
     text = "CGGACTCGACAGATGTGAAGAACGACAATGTGAAG" + \
         "ACTCGACACGACAGAGTGAAGAGAAGAGGAAACATTGTAA"
     assert find_clumps(text, 5, 50, 4) == {'CGACA', 'GAAGA'}
+
+    data_file = open('bioinformatics/tests/clump_finding_data.txt', 'r')
+    # find a better way to handle the path
+    _, text, k, L, t, _, substr = [line[:-1] for line in data_file.readlines()]
+    k, L, t = int(k), int(L), int(t)
+
+    assert find_clumps(text, k, L, t) == set([substr])
