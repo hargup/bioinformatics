43a0adf87a61113850a889b77da7a1a4f7b5aa85
Harsh Gupta <gupta.harsh96@gmail.com>
2014-10-10 01:18:54 +0530

    clumps: prebuild dictionary

--- bioinformatics/clump.py
+++ bioinformatics/clump.py
@@ -1,6 +1,24 @@
 from .frequent_words import compute_frequencies
 
+def all_kerms(k):
+    from itertools import product
+    k_mers = [''.join(bp_tuple) for bp_tuple in product('ATGC', repeat=k)]
+    return k_mers
 
+
+# There are two approaches, either I can find the all the possible k-mers of
+# compute a dictionary of before hand and then update it. The other approach
+# is to create a dictionory on the fly. While iterating over the genome
+# check if we already have that k-mer in the dictonary if not then add that.
+#
+# When k is large the initially computed dictionary can be **very** large.
+# But when generating the dictionary on the fly is expensive because checking if
+# the given k-mer is in the dictionary takes a lot of time.
+#
+# when k is small the second approach is better but it takes forever for large
+# genomes when k is large the first approach takes forever. BTW I think
+# can have cheaper on the fly dictionary creation.
+# On approach can be iterating over the genome one and find all the kemers
 def find_clumps(text, k, L, t):
     """
     Clump Finding Problem: Find patterns forming clumps in a string.
@@ -11,9 +29,15 @@ def find_clumps(text, k, L, t):
     instances of the given k-mer.
     """
     valid_k_mers = set()
-    substring = text[:L]
-    substr_freq = compute_frequencies(substring, k)
-    for substr in substr_freq.keys():
+
+    # intialize frequencies
+    substr_freq = dict([(k_mer, 0) for k_mer in all_kerms(k)])
+
+    # find the frequencies in the first clump
+    for i in range(0, L-k+1):
+        substr = text[i: i+k]
+        substr_freq[substr] += 1
+
         if substr_freq[substr] >= t:
             valid_k_mers.add(substr)
 
@@ -22,15 +46,9 @@ def find_clumps(text, k, L, t):
         added_substr = text[i+L-k: i+L]
         substr_freq[removed_substr] -= 1
 
-        if added_substr in substr_freq.keys():
-            substr_freq[added_substr] += 1
-        else:
-            substr_freq[added_substr] = 1
+        substr_freq[added_substr] += 1
 
         if substr_freq[added_substr] >= t:
             valid_k_mers.add(added_substr)
 
     return valid_k_mers
-
-
-# We can use airspeed veclocity for speed benchmarking
